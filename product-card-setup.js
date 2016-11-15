47040a6bd1ee58149cf08c52a85229ae544b461e
James Redd <jredd@jrdesignhero.com>
2016-11-15 00:03:59 -0800

    load secure animated loading gif

--- product-card-setup.js
+++ product-card-setup.js
@@ -19,7 +19,7 @@ var PageSetup = {
   },
   appendHTML: function () {
     //append the html elements that make up modal
-    var productCardHTML = '<div id="moduleBackground"></div><div id="vApplication" class="reset"><span id="closeBtn">x</span><div id="verizonPhoneDetail"><img id="phoneImage" class="block" /></div><div id="verizonQuickView"><h1 id="phoneName"></h1><div id="starRating"></div><h2 id="summaryTitle">Technical Specs:</h2><img src="http://www.jrdesignhero.com/apps/verizon-project/loading.gif" id="loading-img" /><ul id="summaryList"></ul><h3 id="phonePrice"></h3><p id="phonePriceDetail"></p><a href="#" id="addToCart" class="btn">Add to Cart</a><a href="#" target="_blank" id="viewDetails" class="btn">View More Details</a></div></div>';
+    var productCardHTML = '<div id="moduleBackground"></div><div id="vApplication" class="reset"><span id="closeBtn">x</span><div id="verizonPhoneDetail"><img id="phoneImage" class="block" /></div><div id="verizonQuickView"><h1 id="phoneName"></h1><div id="starRating"></div><h2 id="summaryTitle">Technical Specs:</h2><img src="https://raw.githubusercontent.com/jrdesignhero/jrdesignhero-product-card/master/loading.gif" id="loading-img" /><ul id="summaryList"></ul><h3 id="phonePrice"></h3><p id="phonePriceDetail"></p><a href="#" id="addToCart" class="btn">Add to Cart</a><a href="#" target="_blank" id="viewDetails" class="btn">View More Details</a></div></div>';
     $('body').append(productCardHTML);
   },
   bindUIActions: function () {
